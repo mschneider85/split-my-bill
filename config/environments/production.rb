9178ebacc78806446ed1c6b70ef765c8e2b7a06e
mschneider85 <mschneider85@gmx.de>
2016-07-15 16:21:30 +0200

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'system.splitmybill@gmail.com',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
