7abd08914982c2201eab97a369e151db8460cc00
mschneider85 <mschneider85@gmx.de>
2016-07-20 07:47:53 +0200

    devise translations, redirects on landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to group_path(cookies[:latest_group]) if current_user && current_user.groups.ids.include?(cookies[:latest_group].to_i)
+    if current_user
+      if current_user.groups.ids.include?(cookies[:latest_group].to_i)
+        redirect_to group_path(cookies[:latest_group])
+      else
+        redirect_to groups_path
+      end
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
