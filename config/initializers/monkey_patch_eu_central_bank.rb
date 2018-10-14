c85ba56f55f66b210d8bde7bc2736a86ced16d61
mschneider85 <mschneider85@gmx.de>
2018-10-14 22:27:08 +0200

    Update ruby + monkey patch `eu_central_bank` gem.

---
+++ config/initializers/monkey_patch_eu_central_bank.rb
@@ -0,0 +1,5 @@
+require 'eu_central_bank'
+class EuCentralBank
+  ECB_RATES_URL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'.freeze
+  ECB_90_DAY_URL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'.freeze
+end
